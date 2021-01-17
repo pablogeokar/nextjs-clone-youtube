101ffd3e5909024dd2b7d87366ef33535b2ef771
Pablo George <pablogeokar@hotmail.com>
2021-01-17 07:59:24 -0300

    Config Eslint + Prettier + editorConfig

---
+++ src/pages/_app.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
